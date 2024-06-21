643a0930f28929e98af78e3c5b0e3841bf6d2cf7
Lodovico Ardissao <lodoviko@gmail.com>
2024-06-20 23:18:03 -0300

    20240620 - Desenvolvimento

--- src/app/components/config/estado/estadolist/estadolist.component.ts
+++ src/app/components/config/estado/estadolist/estadolist.component.ts
@@ -1,10 +1,12 @@
-import { Component, ElementRef, ViewChild, inject } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { EstadoService } from '../../../../servico/estado/estado.service';
 import { EstadoDTO } from '../../../../model/estado/estado-dto';
 import { ErrorDTO } from '../../../../model/error/error-dto';
 import Swal from 'sweetalert2';
 import { EstadodetailComponent } from "../estadodetail/estadodetail.component";
 import { PaginacaoDTO } from '../../../../model/paginacao/paginacao-dto';
+import { EMPTY } from 'rxjs';
+
 
 @Component({
     selector: 'app-estadolist',
@@ -26,6 +28,10 @@ export class EstadolistComponent {
   nomeDaModal!: string;
 
   // Variáveis para controlar a paginação
+
+  paginaItens: number[] = [];
+  paginaItensTemp: number[] = [];
+  paginaDestaque: string[] = [];
 
   paginaAtual: number = 0;
 
@@ -61,7 +67,7 @@ export class EstadolistComponent {
   QtdeElementosPorPagina: number = 10;
 
   constructor() {
-    this.processarPagina(0);
+    this.processarPagina(0, 1);
     // this.estadosListar(0, this.QtdeElementosPorPagina);
   }
 
@@ -70,15 +76,7 @@ export class EstadolistComponent {
       next: sucesso => {
         this.estados = sucesso.content;
         this.paginacaoDTO = sucesso.pageable;
-        console.log('Página número          : ' + this.paginacaoDTO.pageNumber);
-        console.log('Qtde por página        : ' + this.paginacaoDTO.pageSize);
-        console.log('Qtde de página         : ' + this.paginacaoDTO.totalPages);
-        console.log('Qtde total de registros: ' + this.paginacaoDTO.totalElements);
-        console.log('É primeira página      : ' + this.paginacaoDTO.firstPage);
-        console.log('É última página        : ' + this.paginacaoDTO.lastPage);
-
         this.paginacaoDetalhes();
-
       },
       error: erros => {
         this.errorDTO = erros.error;
@@ -138,95 +136,77 @@ export class EstadolistComponent {
           });
         }
       });
+   }
+
+  fecharModal() {
+    this.estadosListar(0, this.QtdeElementosPorPagina);
+  }
+
+  paginacaoDetalhes() {
+    // this.paginaItens = Array(this.paginacaoDTO.totalPages).fill(0).map((x,i) => i);
+
+    let primeiroItemExibir = 0;
+    let ultimoItemExibir = this.paginacaoDTO.totalPages
+
+    if(this.paginacaoDTO.pageNumber > 3 && this.paginacaoDTO.totalPages > 7) {
+      primeiroItemExibir = this.paginacaoDTO.pageNumber - 3;
     }
 
-    fecharModal() {
-      this.estadosListar(0, this.QtdeElementosPorPagina);
-    }
+    this.paginaItens = Array(ultimoItemExibir - primeiroItemExibir);
 
-    paginacaoDetalhes() {
-      this.paginaPrimeira = this.paginacaoDTO.firstPage? 'disabled desabilitarClic': '';
-      this.paginaUltima = this.paginacaoDTO.lastPage? 'disabled desabilitarClic': '';
 
-      // let pgTemp = Math.trunc(this.paginacaoDTO.totalPages / 7);
-
-      // console.log('parte: ' + pgTemp);
-
-      this.montarPagina();
-
-    }
-
-    processarPagina(pagina: number) {
-
-      console.log('-----> ' + this.QtdeElementosPorPagina);
-
-      if(pagina == 0) {  //  Clicou para Primeira página
-        this.paginaAtual = 0;
-      } else if(pagina == -9) { // Clicou para última página
-        this.paginaAtual = this.paginacaoDTO.totalPages - 1;
-      } else if(pagina == -1) { // Clicou para voltar uma página
-        this.paginaAtual = this.paginacaoDTO.pageNumber - 1;
-      } else if(pagina == -2) { // Clicou para avançar uma pagina
-        this.paginaAtual = this.paginacaoDTO.pageNumber + 1;
-      } else if(pagina >= 0) {
-        this.paginaAtual = pagina;
+    for(let i = 0; i < ultimoItemExibir; i++) {
+     if(i >= primeiroItemExibir) {
+        this.paginaItensTemp[i] = i;
       }
 
-      this.estadosListar(this.paginaAtual, this.QtdeElementosPorPagina);
-
-      this.paginaAtiva(this.paginaAtual);
-
-      // for(let i=this.paginacaoDTO.pageNumber; i <= this.paginacaoDTO.totalPages; i++) {
-
-      // }
-
     }
 
-    montarPagina() {
+    this.paginaItens = this.paginaItensTemp.filter(n => n >= primeiroItemExibir);
 
-      console.log('Montando a página................ a página atual é: ' + this.paginaAtual);
-
-      this.ocultarItem1 = this.ocultarItem2 = this.ocultarItem3 = this.ocultarItem4 = this.ocultarItem5 = this.ocultarItem6 =  this.ocultarItem7 =  false;
-
-      if(this.paginacaoDTO.totalPages <=7) {
-
-        this.numPageItem1 = 1; this.numPageItem2 = 2; this.numPageItem3 = 3; this.numPageItem4 = 4; this.numPageItem5 = 5; this.numPageItem6 = 6; this.numPageItem7 = 7;
-
-        this.ocultarItem2 = this.paginacaoDTO.totalPages < 2;
-        this.ocultarItem3 = this.paginacaoDTO.totalPages < 3;
-        this.ocultarItem4 = this.paginacaoDTO.totalPages < 4;
-        this.ocultarItem5 = this.paginacaoDTO.totalPages < 5;
-        this.ocultarItem6 = this.paginacaoDTO.totalPages < 6;
-        this.ocultarItem7 = this.paginacaoDTO.totalPages < 7;
+    for(let k = 0; k < this.paginaItens.length; k++) {
+      if(this.paginaAtual == this.paginaItens[k]) {
+        this.paginaDestaque[k] = 'active desabilitarClic'
       } else {
-        // TODO desenvolver
-
-        let i = 0;
-
-          if(this.paginaAtual > 6) {
-            i = this.paginaAtual;
-
-            if(i-7 >= 0 ) {
-              this.numPageItem1 = this.paginaAtual - 7; this.numPageItem2 = this.paginaAtual - 6; this.numPageItem3 = this.paginaAtual - 5; this.numPageItem4 = this.paginaAtual - 4; this.numPageItem5 = this.paginaAtual - 3; this.numPageItem6 = this.paginaAtual - 2; this.numPageItem7 = this.paginaAtual - 1;
-            }
-          }
-
+        this.paginaDestaque[k] = '';
       }
     }
 
-    obterSelect(e:any) {
-      this.QtdeElementosPorPagina = e.target.value;
-      this.processarPagina(0);
-    }
+    this.paginaPrimeira = this.paginacaoDTO.firstPage? 'disabled desabilitarClic': '';
+    this.paginaUltima = this.paginacaoDTO.lastPage? 'disabled desabilitarClic': '';
 
-    paginaAtiva(pagina: number) {
-      // active
-      this.paginaItem1 = pagina == 0? 'active desabilitarClic': '' ;
-      this.paginaItem2 = pagina == 1? 'active desabilitarClic': '' ;
-      this.paginaItem3 = pagina == 2? 'active desabilitarClic': '' ;
-      this.paginaItem4 = pagina == 3? 'active desabilitarClic': '' ;
-      this.paginaItem5 = pagina == 4? 'active desabilitarClic': '' ;
-      this.paginaItem6 = pagina == 5? 'active desabilitarClic': '' ;
-      this.paginaItem7 = pagina == 6? 'active desabilitarClic': '' ;
+  }
+
+  processarPrimeiraPagina(): void {
+     this.processarPagina(0, 1);
+  }
+
+  processarUltimaPagina(): void {
+    this.processarPagina(this.paginacaoDTO.totalPages - 1, 7);
+  }
+
+  processarVoltarPagina(): void {
+    if(this.paginacaoDTO.pageNumber > 0) {
+      this.processarPagina(this.paginacaoDTO.pageNumber - 1, 1);
     }
+  }
+
+  processarAvancarPagina(): void {
+    if(this.paginacaoDTO.pageNumber < this.paginacaoDTO.totalPages) {
+      this.processarPagina(this.paginacaoDTO.pageNumber + 1, 7);
+    }
+  }
+
+  processarPagina(pagina: number, item: number) {
+    this.paginaAtual = pagina;
+
+    this.estadosListar(this.paginaAtual, this.QtdeElementosPorPagina);
+
+  }
+
+  obterSelect(e:any) {
+    this.QtdeElementosPorPagina = e.target.value;
+    this.processarPagina(0, 1);
+  }
+
 }
